9c8133a0ac49ba52643325a4d54b38c6f51eb35b
pistoled <yuri_matteus@hotmail.com>
2025-03-27 14:24:07 -0300

    movExp criado, Default Button criado, FormField refatorado, integrando funções nativas do vue

--- src/components/MovExp.vue
+++ src/components/MovExp.vue
@@ -1,8 +1,35 @@
 <template>
   <div class="movExp">
+   <div>
     <p>Movimentação do dia</p>
+
+    <ul>
+      <li>
+        Calça
+      </li>
+    </ul>
+    <ul>
+      <li>
+        R$: 22
+      </li>
+    </ul>
+   </div>
+   <div>
     <p >Relação de Despesas</p>
+    <ul>
+      <li>
+        Calça
+      </li>
+    </ul>
+    <ul>
+      <li>
+        R$: 22
+      </li>
+    </ul>
+   </div>
+
   </div>
+
 </template>
 
 <script lang="ts">
@@ -26,6 +53,13 @@ export default defineComponent({
     font-weight: bolder;
   }
 }
-
+li{
+  list-style: none;
+  color: #006178;
+}
+ul{
+  display: inline-block;
+  margin-right: 95px;
+}
 
 </style>
